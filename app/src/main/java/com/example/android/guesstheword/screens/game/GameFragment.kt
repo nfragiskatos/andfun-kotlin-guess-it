0adf3bc236007f15d9ea5c9ba3e32a5b7f667efd
Nicholas Fragiskatos <nfragiskatos@outlook.com>
2019-10-12 14:18:42 -0700

    Directly bound the score and word text field to the GameViewModel, and removed the manual observers in the GameFragment.

--- app/src/main/java/com/example/android/guesstheword/screens/game/GameFragment.kt
+++ app/src/main/java/com/example/android/guesstheword/screens/game/GameFragment.kt
@@ -56,14 +56,7 @@ class GameFragment : Fragment() {
         viewModel = ViewModelProviders.of(this).get(GameViewModel::class.java)
 
         binding.gameViewModel = viewModel
-
-        viewModel.score.observe(this, Observer { newScore ->
-            binding.scoreText.text = newScore.toString()
-        })
-
-        viewModel.word.observe(this, Observer { newWord ->
-            binding.wordText.text = newWord;
-        })
+        binding.lifecycleOwner = this
 
         viewModel.eventGameFinished.observe(this, Observer { hasFinished ->
             if (hasFinished) {
