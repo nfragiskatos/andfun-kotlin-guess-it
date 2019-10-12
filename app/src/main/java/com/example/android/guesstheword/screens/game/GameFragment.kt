4295757e6e2d764a2b174e15db21ebc05addc95e
Nicholas Fragiskatos <nfragiskatos@outlook.com>
2019-10-12 11:25:50 -0700

    Added data GameViewModel variable in game_fragment.xml so it can communicate directly with the viewModel directly, and then set the android:onClick attribute for the buttons with a lambda that will call the appropriate GameViewModel methods directly.
    In GameFragment I bound the GameViewModel to the layout using the data binding object.
    
    Was able to remove the onClick listeners from my GameFragment.
    
    Added gameViewModel to my game_fragment.xml

--- app/src/main/java/com/example/android/guesstheword/screens/game/GameFragment.kt
+++ app/src/main/java/com/example/android/guesstheword/screens/game/GameFragment.kt
@@ -54,12 +54,8 @@ class GameFragment : Fragment() {
 
         Log.i("GameFragment", "Called ViewModelProviders.of!")
         viewModel = ViewModelProviders.of(this).get(GameViewModel::class.java)
-        binding.correctButton.setOnClickListener {
-            viewModel.onCorrect()
-        }
-        binding.skipButton.setOnClickListener {
-            viewModel.onSkip()
-        }
+
+        binding.gameViewModel = viewModel
 
         viewModel.score.observe(this, Observer { newScore ->
             binding.scoreText.text = newScore.toString()
